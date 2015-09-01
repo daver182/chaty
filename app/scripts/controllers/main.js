e00c45f873599ee48cca0e2cbdc5e1df9241adc2
Daniel Vergara Urrutia <daver182@gmail.com>
2015-09-01 10:33:32 -0300

    Se eliminaron las otras formas de login, problemas con Electron

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,16 +8,5 @@
  * Controller of the chatApp
  */
 angular.module('chatApp').controller('MainCtrl', function ($scope, Auth, $location) {
-    $scope.oauthLogin = function(provider) {
-		$scope.err = null;
-		Auth.$authWithOAuthPopup(provider, { rememberMe: true }).then(redirect, showError);
-    };
 
-    function redirect() {
-		$location.path('/chat');
-    }
-
-    function showError(err) {
-		$scope.err = err;
-    }
 });
