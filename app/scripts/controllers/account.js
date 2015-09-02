98f3f5b08a20c27def7d4c6af94f36db2bd5002c
Daniel Vergara Urrutia <daver182@gmail.com>
2015-09-02 11:24:20 -0300

    Se corrigio un error que no permitia cambiar de usuario

--- app/scripts/controllers/account.js
+++ app/scripts/controllers/account.js
@@ -10,17 +10,15 @@ angular.module('chatApp').controller('AccountCtrl', function ($scope, user, Auth
 	$scope.user = user;
 	
 	blockUI.start();
-	var profile = $firebaseObject(Ref.child('users/' + user.uid)).$loaded().then(dataLoaded).catch(error);
+
+	$firebaseObject(Ref.child('users/' + user.uid)).$bindTo($scope, 'profile').then(function(){
+		blockUI.stop();
+	}).catch(error);
 
 	$scope.logout = function() {
 		Auth.$unauth();
 		$location.path('/');
 	};
-
-	function dataLoaded(data){
-		$scope.profile = data;
-		blockUI.stop();
-	}
 
 	function error(err){
 		console.log(err);
