de7f68216497db819cdb957ff63be5cac4012a23
Daniel Vergara Urrutia <daver182@gmail.com>
2015-09-02 12:51:40 -0300

    Se utiliza un CDN para el locale de moment

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -9,19 +9,19 @@
  * Main module of the application.
  */
 angular.module('chatApp', [
-    'ngCookies',
-    'ngMessages',
-    'ngResource',
-    'ngRoute',
-    'ngSanitize',
-    'ngTouch',
-    'firebase',
-    'firebase.ref',
-    'firebase.auth',
-    'blockUI',
-    'angularMoment'
-  ]).config(function(blockUIConfig){
-    blockUIConfig.message = 'Cargando...';
-  }).run(function(amMoment){
-    amMoment.changeLocale('es');
+        'ngCookies',
+        'ngMessages',
+        'ngResource',
+        'ngRoute',
+        'ngSanitize',
+        'ngTouch',
+        'firebase',
+        'firebase.ref',
+        'firebase.auth',
+        'blockUI',
+        'angularMoment'
+    ]).config(function(blockUIConfig){
+        blockUIConfig.message = 'Cargando...';
+    }).run(function(amMoment){
+        amMoment.changeLocale('es');
 });
