b2f21a42bee18ae5105279b7f90aff2db25637fb
Daniel Vergara Urrutia <daver182@gmail.com>
2015-09-09 19:14:29 -0300

    Se modificaron las conexiones para usar las que provee angularfire

--- app/scripts/services/notification.js
+++ app/scripts/services/notification.js
@@ -7,22 +7,28 @@
  * # notification
  * Service in the chatApp.
  */
-angular.module('chatApp').service('notification', function (profile, webNotification) {
-	var stateRef = new Firebase('https://zbox-chat.firebaseio.com/notification');
+angular.module('chatApp').service('notification', function (profile, webNotification, Ref, $firebaseObject) {
+	//var stateRef = new Firebase('https://zbox-chat.firebaseio.com/notification');
+	
+	var lastNotification = $firebaseObject(Ref.child('notification'));
+	var first = true;
 
-	stateRef.on('value', function(snapshot) {
-		console.log(snapshot.val());
-		var data = snapshot.val();
+	lastNotification.$watch(function(data) {
+		console.log('watch', data);
+		if(first) {first = false; return;}
 		
-		if(!data) return;
-		if(data.user.id === profile.getId()) return;
-		
-		if(data.type === 'online') {
-			newUser(data.user.name);
-		} else if(data.type === 'message') {
-			newMessage(data.user.name);
+
+		console.log(lastNotification);
+		if(!lastNotification) return;
+		if(lastNotification.user.id === profile.getId()) return;
+
+		if(lastNotification.type === 'online') {
+			newUser(lastNotification.user.name);
+		} else if(lastNotification.type === 'message') {
+			newMessage(lastNotification.user.name);
 		}
 	});
+	
 
     function newUser(name){
     	var message = 'El usuario ' + name + ' se ha conectado al chat';
@@ -40,16 +46,19 @@ angular.module('chatApp').service('notification', function (profile, webNotifica
 			icon: 'images/logo.png',
 			autoClose: 4000
 		}, function onShow(error, hide) {
-			if (!error) {
-				console.log('Se mostrado correctamente la notificacion');
+			if (error) {
+				console.log(error);
 			}
 		});
     }
 
     return {
     	create: function(uid, type){
+    		console.log('create');
     		profile.getProfile().then(function(user){
-				stateRef.set({ user: { id: profile.getId(), name: user.name }, type: type, time: Firebase.ServerValue.TIMESTAMP });
+    			var notification = $firebaseObject(Ref.child('notification'));
+    			notification.$value = { user: { id: profile.getId(), name: user.name }, type: type, time: new Date().valueOf() };
+    			notification.$save();
 			});
     	}
     }
--- app/scripts/services/online.js
+++ app/scripts/services/online.js
@@ -8,18 +8,24 @@
  * Service in the chatApp.
  */
 angular.module('chatApp').service('online', function ($firebaseObject, Ref, notification) {
-	var amOnline = new Firebase('https://zbox-chat.firebaseio.com/.info/connected');
+	var onlineStatus = $firebaseObject(Ref.child('.info').child('connected'));
+	var connected = false;
 
 	return {
 		on: function(uid, cb){
-			var userRef = new Firebase('https://zbox-chat.firebaseio.com/users/' + uid +'/online');
-			
-			amOnline.on('value', function(snapshot) {
-				if (snapshot.val()) {
+			var userRef = Ref.child('users').child(uid).child('online');
+			var userStatus = $firebaseObject(userRef);
+
+			onlineStatus.$watch(function(data) {
+				if(data.key === 'connected' && !connected){
+					connected = true;
 					userRef.onDisconnect().set(false);
-					userRef.set(true);
+					userStatus.$value = true;
+					userStatus.$save();
 
 					notification.create(uid, 'online');
+				}else{
+					connected = false;
 				}
 			});
 		}
